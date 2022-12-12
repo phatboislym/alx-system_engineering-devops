b9b040b41da8738de7c35fca2e28d920aefb652e
Adewole <phatboislym@protonmail.com>
2022-12-13 00:45:52 +0100

    add: advanced task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# client SSH configuration file
+file_line { 'Change the main private key':
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/school',
+}
+
+file_line { 'No Authenticate with passowrd':
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
+}
