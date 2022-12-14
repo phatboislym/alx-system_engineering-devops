40c7b3dd368e7df3dda33682f4dbe5c7e56b1b95
Adewole <phatboislym@protonmail.com>
2022-12-14 23:34:25 +0100

    fix: update script

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,11 @@
+# Installs an Nginx server using puppet
+# Requirements:
+# nginx should be listening on port 80
+# when querying Nginx at its root / with a GET request (requesting a page)
+#+ using curl, it must return a page that contains the string Hello World!
+# the redirection must be a “301 Moved Permanently”
+
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https://www.nginx.com/products/nginx-management-suite/ permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+}
