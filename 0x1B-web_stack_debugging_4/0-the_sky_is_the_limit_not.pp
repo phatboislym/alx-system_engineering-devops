a7f0460a289de1526c74e2499ec5c3f31e4cb9c9
Adewole <phatboislym@protonmail.com>
2023-02-09 21:29:39 +0100

    fix: update task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,9 @@
 $file = '/etc/nginx/nginx.conf'
 
-
 exec { 'edit_in_place':
   command => "sed -i '2s/4/auto/' ${file}",
   path    => '/usr/bin:/usr/sbin:/bin:/sbin',
-  notify  => Exec['nginx_restart']
+  notify  => Exec['restart_nginx']
 }
 
 exec { 'restart_nginx':
